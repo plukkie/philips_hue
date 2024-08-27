14ac6aac45ad1b6ea84e8e984ef3f0ac481ac3fc
Plukkie <44134205+plukkie@users.noreply.github.com>
2024-08-27 09:29:46 +0200

    finalize with single light and grouped_light

--- huev2.sh
+++ huev2.sh
@@ -6,26 +6,45 @@
 ############################################################
 
 # BEGIN CONSTANTS
-#huebridge='https://10.100.102.138'
 huebridge='https://192.168.0.103'
 api_key_file='hue_api_key.txt'
 BK_wipper_ID=b7d7eb18-4382-4cad-9d1d-b58d49ee70fc
 BK_lightstrip_ID=aa371cce-34b0-4a0f-aaf7-2c79fbed6914
+Keuken_wipper_ID=47e42e2f-1f63-46f5-a3b3-890e9c908cb7
+Keuken_Kast_ID=b69b6407-cf7e-4a66-85ad-ddefc298cebd  # Grouped light ID
 # END CONSTANTS
 
-# Badkamer spot src = 1
-# Badkamer lightstrip kast = 2
-function check_lights { src_lightid=$BK_wipper_ID; dst_lightid=$BK_lightstrip_ID;
+function check_lights () {
+	
+    # declare before function vars:
+    # src_lightid=<HUE ID>
+    # dst_lightid=<HUE ID of destination light or group>
+    # dst=group (optional of dealing with a grouped_light)
+    
+    header="hue-application-key: "$user
+    api_dst_light="light"
+    
+    # If it is a group, change API
+    if [[ $dst == 'group' ]]; then api_dst_light="grouped_light"; fi
+
 	# Function that gets status of src lamp
-	header="hue-application-key: "$user
 	resp=`curl -k -s -H "$header" X GET $huebridge/clip/v2/resource/light/$src_lightid`
 	#resp_all_lights=`curl -k -s -H "$header" X GET $huebridge/clip/v2/resource/light`
-	#echo $resp_all_lights | jq
-	src_on=`echo $resp | jq .data.[].on.on`
-	src_name=`echo $resp | jq .data.[].metadata.name`
-	resp=`curl -k -s -H "$header" -X GET $huebridge/clip/v2/resource/light/$dst_lightid`
-	dst_on=`echo $resp | jq -r .data.[].on.on`
-	dst_name=`echo $resp | jq .data.[].metadata.name`
+    #echo $resp_all_lights | jq
+	src_on=`echo $resp | jq .data[].on.on` #get status
+	src_name=`echo $resp | jq .data[].metadata.name` #get name
+	resp=`curl -k -s -H "$header" -X GET $huebridge/clip/v2/resource/$api_dst_light/$dst_lightid`
+	dst_on=`echo $resp | jq -r .data[].on.on` #get status
+
+    if [[ $dst == "group" ]]; then
+        # actions when to turn on a group of lights
+        dst_room_id=`echo $resp | jq -r '.data[] | select(.id == "'"$dst_lightid"'") | .owner.rid'` #get ID of room the group belongs to
+        resp=`curl -k -s -H "$header" -X GET $huebridge/clip/v2/resource/room/$dst_room_id`
+        dst_name=`echo $resp | jq -r '.data[] | select(.id=="'"$dst_room_id"'") | .metadata.name'`" (group)" #get room name
+    else
+        dst_name=`echo $resp | jq .data[].metadata.name` #get name of single light
+    fi
+
 	echo
 	echo " Light $src_lightid:" 
 	echo "  - on:        "$src_on
@@ -43,7 +62,7 @@ function check_lights { src_lightid=$BK_wipper_ID; dst_lightid=$BK_lightstrip_ID
 			echo "Light $src_lightid is turned on and reachable!"
 			echo "Will activate light $dst_lightid..."
 			echo
-			resp=`curl -k -s -H "$header" -X PUT -d '{ "on" : { "on" : true }}' $huebridge/clip/v2/resource/light/$dst_lightid`
+			resp=`curl -k -s -H "$header" -X PUT -d '{ "on" : { "on" : true }}' $huebridge/clip/v2/resource/$api_dst_light/$dst_lightid`
 		fi
 	elif [[ $dst_on == true ]]; then
 		# Source light is not shining and dst light is switched on
@@ -51,10 +70,9 @@ function check_lights { src_lightid=$BK_wipper_ID; dst_lightid=$BK_lightstrip_ID
 		echo
 		echo "Will switch off light $dst_lightid..."
         echo
-        resp=`curl -k -s -H "$header" -X PUT -d '{ "on" : { "on" : false }}' $huebridge/clip/v2/resource/light/$dst_lightid`
+        resp=`curl -k -s -H "$header" -X PUT -d '{ "on" : { "on" : false }}' $huebridge/clip/v2/resource/$api_dst_light/$dst_lightid`
 	fi
 }
-
 
 if ! test -f $api_key_file; then
 	# API keyfile not found -> generate API key
@@ -77,15 +95,24 @@ if grep -q error "$api_key_file"; then
 	fi
 else
 	# Get APi username
-        echo 'Get API details from file...'
+    echo 'Get API details from file...'
 	user=`cat $api_key_file | jq -r .[].success.username`
-        clientkey=`cat $api_key_file | jq -r .[].success.clientkey`
-        echo
+    clientkey=`cat $api_key_file | jq -r .[].success.clientkey`
+    echo
 	echo ' API user: ' $user
-        echo ' API clientkey: ' $clientkey
-        echo ' IP HUE bridge: ' $huebridge 
-        echo
+    echo ' API clientkey: ' $clientkey
+    echo ' IP HUE bridge: ' $huebridge 
+    echo
 fi
 
+# Check lights of bathroom for bathroom closet
+src_lightid=$BK_wipper_ID
+dst_lightid=$BK_lightstrip_ID
+check_lights
+
+# Check light of kitchen for kitchen closet
+src_lightid=$Keuken_wipper_ID
+dst_lightid=$Keuken_Kast_ID
+dst=group
 check_lights
 
