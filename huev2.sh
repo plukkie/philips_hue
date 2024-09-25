c6fdceb766819b433d3c734e49d8aff3cfb0485f
Plukkie <44134205+plukkie@users.noreply.github.com>
2024-09-25 09:13:31 +0200

    Update huev2.sh

--- huev2.sh
+++ huev2.sh
@@ -6,12 +6,25 @@
 ############################################################
 
 # BEGIN CONSTANTS
-huebridge='https://192.168.0.103'
+huebridge='https://A.B.C.D'
 api_key_file='hue_api_key.txt'
-BK_wipper_ID=b7d7eb18-4382-4cad-9d1d-b58d49ee70fc
+off_delay=3600 #seconds
+script_interval=20 # Seconds
+BK_wipper_ID=430777b5-dd47-481c-9f46-139548276b56
 BK_lightstrip_ID=aa371cce-34b0-4a0f-aaf7-2c79fbed6914
 Keuken_wipper_ID=47e42e2f-1f63-46f5-a3b3-890e9c908cb7
 Keuken_Kast_ID=b69b6407-cf7e-4a66-85ad-ddefc298cebd  # Grouped light ID
+
+BK_All_Bulb_IDs=( "b7d7eb18-4382-4cad-9d1d-b58d49ee70fc" \
+                  "0836977d-eb51-44ce-aa99-23139d66920b" \
+                  "56221424-7fe9-4a3b-968e-2e338c89320a" \
+                  "6807c7f5-ed3e-415b-8753-f8a83a5251b1" \
+                  "b67d1feb-46e5-4816-bddb-8da1815df37f" \
+                  "d25fe339-443e-404b-aa2d-23e77815f29e" \
+                  "e8373324-5ae0-4604-8170-b02356f863f7" )
+
+BK_spot=0836977d-eb51-44ce-aa99-23139d66920b
+
 # END CONSTANTS
 
 function check_lights () {
@@ -27,15 +40,26 @@ function check_lights () {
     # If it is a group, change API
     if [[ $dst == 'group' ]]; then api_dst_light="grouped_light"; fi
 
-	# Function that gets status of src lamp
-	resp=`curl -k -s -H "$header" X GET $huebridge/clip/v2/resource/light/$src_lightid`
+
+	# Get status of src lamp(s)
 	#resp_all_lights=`curl -k -s -H "$header" X GET $huebridge/clip/v2/resource/light`
-    #echo $resp_all_lights | jq
-	src_on=`echo $resp | jq .data[].on.on` #get status
-	src_name=`echo $resp | jq .data[].metadata.name` #get name
+    #echo $resp_all_lights | jq > all_lights.json
+    resp=`curl -k -s -H "$header" X GET $huebridge/clip/v2/resource/light/$src_lightid`
+    #echo $resp | jq
+    #src_rid=`echo $resp | jq -r '.data[] | select(.id == "'"$src_lightid"'") | .owner.rid'` #get ID of src light 
+    #echo $src_rid
+    #zigbee_data=`curl -k -s -H "$header" X GET $huebridge/clip/v2/resource/zigbee_connectivity/$src_rid`
+    
+    #echo $zigbee_data | jq
+    #echo $resp | jq
+    src_on=`echo $resp | jq .data[].on.on` #get status
+    src_name=`echo $resp | jq .data[].metadata.name` #get name
+
+    # Get status of dst lamp
 	resp=`curl -k -s -H "$header" -X GET $huebridge/clip/v2/resource/$api_dst_light/$dst_lightid`
 	dst_on=`echo $resp | jq -r .data[].on.on` #get status
 
+    # Get name of dst light or group
     if [[ $dst == "group" ]]; then
         # actions when to turn on a group of lights
         dst_room_id=`echo $resp | jq -r '.data[] | select(.id == "'"$dst_lightid"'") | .owner.rid'` #get ID of room the group belongs to
@@ -46,31 +70,47 @@ function check_lights () {
     fi
 
 	echo
-	echo " Light $src_lightid:" 
+	echo " Source Light $src_lightid:" 
 	echo "  - on:        "$src_on
 	echo "  - name:      "$src_name
 	echo
-	echo " Light $dst_lightid:" 
+	echo " Destination Light $dst_lightid:" 
 	echo "  - on:        $dst_on"
 	echo "  - name:      "$dst_name
+    echo "  - dst:       "$dst
 	echo
 	
 	if [[ $src_on == true ]]; then
 		# Src light is shining
 		if [[ $dst_on == false ]]; then 
-			# Switch on dst light
+			# Dst light is off, lets turn ON!
 			echo "Light $src_lightid is turned on and reachable!"
 			echo "Will activate light $dst_lightid..."
 			echo
 			resp=`curl -k -s -H "$header" -X PUT -d '{ "on" : { "on" : true }}' $huebridge/clip/v2/resource/$api_dst_light/$dst_lightid`
-		fi
+            if ! [ -f $dst_lightid.started ]; then echo 0 > $dst_lightid.started ; fi #create startfile for dst light
+		else
+            # Dst light is shining, reset timer to 0
+            X=$(< $dst_lightid.started)
+            if (( $X !=0 )); then
+                echo 0 > $dst_lightid.started
+            fi
+        fi
 	elif [[ $dst_on == true ]]; then
 		# Source light is not shining and dst light is switched on
-		# Need to switch off dst light now
-		echo
-		echo "Will switch off light $dst_lightid..."
-        echo
-        resp=`curl -k -s -H "$header" -X PUT -d '{ "on" : { "on" : false }}' $huebridge/clip/v2/resource/$api_dst_light/$dst_lightid`
+        X=$(< $dst_lightid.started)
+        if (( $X < $off_delay )); then
+            # Need to wait till delay time has finished
+            Y=$(($X+$script_interval))
+            echo $Y > $dst_lightid.started
+        else
+		    # Need to switch off dst light now
+		    echo
+		    echo "Will switch off light $dst_lightid after $off_delay secs..."
+            echo
+            resp=`curl -k -s -H "$header" -X PUT -d '{ "on" : { "on" : false }}' $huebridge/clip/v2/resource/$api_dst_light/$dst_lightid`
+            echo 0 > $dst_lightid.started
+        fi
 	fi
 }
 
@@ -102,10 +142,12 @@ else
 	echo ' API user: ' $user
     echo ' API clientkey: ' $clientkey
     echo ' IP HUE bridge: ' $huebridge 
+    echo ' Turn off delay: ' $off_delay seconds
     echo
 fi
 
 # Check lights of bathroom for bathroom closet
+dst=single
 src_lightid=$BK_wipper_ID
 dst_lightid=$BK_lightstrip_ID
 check_lights
@@ -115,4 +157,3 @@ src_lightid=$Keuken_wipper_ID
 dst_lightid=$Keuken_Kast_ID
 dst=group
 check_lights
-
